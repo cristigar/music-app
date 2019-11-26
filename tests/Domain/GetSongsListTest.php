6b451cbd049db0b703d7a3b7de1e95f5fca7d8fd
DimaSocolovschi <event.horizont3@gmail.com>
2019-11-26 16:03:55 +0200

    GetSongs function and test for GetSongs function added

---
+++ tests/Domain/GetSongsListTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../../src/Domain/GetSongsList.php';
+
+function testGetSongsList() {
+    $songs = getSongs();
+
+    $result = $songs === [
+        ['title' => 'Title 1', 'artist' => 'Artist 1', 'genre' => 'Genre 1'],
+        ['title' => 'Title 2', 'artist' => 'Artist 2', 'genre' => 'Genre 2'],
+        ['title' => 'Title 3', 'artist' => 'Artist 3', 'genre' => 'Genre 3'],
+        ['title' => 'Title 4', 'artist' => 'Artist 4', 'genre' => 'Genre 3'],
+        ['title' => 'Title 5', 'artist' => 'Artist 5', 'genre' => 'Genre 2'],
+        ['title' => 'Title 6', 'artist' => 'Artist 6', 'genre' => 'Genre 3'],
+    ];
+
+    var_dump($result);
+}
+
+testGetSongsList();
